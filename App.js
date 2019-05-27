717f81b3ef4f30d27ebb5e9e198c1faf4a7b2258
Elvis Douglas Janegitz <edjelvis@gmail.com>
2019-05-26 22:34:42 -0300

    Adicionando calendario

--- App.js
+++ App.js
@@ -1,26 +1,19 @@
 import React from 'react'
-import { View, StyleSheet } from 'react-native'
+import { View } from 'react-native'
 import AddEntry from './components/AddEntry'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
-        <View style={styles.container}>
-          <AddEntry />
+        <View style={{flex: 1}}>
+          <History />
         </View>
       </Provider>
     )
   }
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-})
